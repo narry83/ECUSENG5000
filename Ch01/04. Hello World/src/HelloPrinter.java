c7f66229c335cec2e7423cb6d0b8cddd05bca3a1
nsnaraya <nsnaraya@OITEAS-005F18.oitclients.ncsu.edu>
2019-09-24 15:48:45 -0400

    test

--- Ch01/04. Hello World/src/HelloPrinter.java
+++ Ch01/04. Hello World/src/HelloPrinter.java
@@ -1,5 +1,9 @@
 public class HelloPrinter
 {
+	
+	//String Hello="Hello, World!";
+	//int number=10;
+	
    public static void main(String[] args)
    {
       // Display a greeting in the console window
