b2dae3e20327e39006d370781bf8499dd0285e1b
nsnaraya <nsnaraya@OITEAS-005F18.oitclients.ncsu.edu>
2019-10-18 08:27:47 -0400

    1017

--- WileyBook/Ch03/src/KW/CH03/GetSentence.java
+++ WileyBook/Ch03/src/KW/CH03/GetSentence.java
@@ -5,40 +5,44 @@ import java.util.StringJoiner;
 
 /**
  * Class to demonstrate use of debugger
+ * 
  * @author Koffman & Wolfgang
  */
 public class GetSentence {
-    /*<exercise chapter="3" section="7" type="programming" number="1">*/
-    public static final boolean TESTING = true;
-    /*<exercise>*/
-    /*<listing chapter="3" number="2">*/
-    /**
-     * Return the individual words entered by the user.
-     * The user can enter the sentinel *** to terminate data entry.
-     * @return A string with a maximum of 10 words
-     */
-    public static String getSentence() {
-        Scanner in = new Scanner(System.in);
-        StringJoiner stb = new StringJoiner(" ");
-        int count = 0;
-        while (count < 10) {
-            System.out.println("Enter a word or *** to quit");
-            String word = in.next();
-            /*<exercise chapter="3" section="7" type="programming" number="1">*/
-            if (TESTING) {                 
-            if (word.equals("***")) break;
-            System.out.println("!!! Next word is " + word + ", count is " + count);	
-            stb.add(word);
-            count++;
-            }
-        }    
-        return stb.toString();
-    }
-    /*</listing>*/
-    
-    public static void main(String[] args) {
-        String sentence = getSentence();
-        System.out.println("The sentence is " + sentence);
-    }
-    
+	/* <exercise chapter="3" section="7" type="programming" number="1"> */
+	public static final boolean TESTING = true;
+
+	/* <exercise> */
+	/* <listing chapter="3" number="2"> */
+	/**
+	 * Return the individual words entered by the user. The user can enter the
+	 * sentinel *** to terminate data entry.
+	 * 
+	 * @return A string with a maximum of 10 words
+	 */
+	public static String getSentence() {
+		Scanner in = new Scanner(System.in);
+		StringJoiner stb = new StringJoiner(" ");
+		int count = 0;
+		while (count < 10) {
+			System.out.println("Enter a word or *** to quit");
+			String word = in.next();
+			/* <exercise chapter="3" section="7" type="programming" number="1"> */
+			if (TESTING) {
+				if (word.equals("***"))
+					break;
+				System.out.println("!!! Next word is " + word + ", count is " + count);
+				stb.add(word);
+				count++;
+			}
+		}
+		return stb.toString();
+	}
+	/* </listing> */
+
+	public static void main(String[] args) {
+		String sentence = getSentence();
+		System.out.println("The sentence is " + sentence);
+	}
+
 }
--- WileyBook/Ch03/src/KW/CH03/MyInput.java
+++ WileyBook/Ch03/src/KW/CH03/MyInput.java
@@ -7,60 +7,57 @@ import java.util.Scanner;
 
 public class MyInput {
 
-    /*<listing chapter="A" number="6">*/
-    /**
-     * Method to return an integer data value.
-     * @param prompt Message
-     * @return The data value read as an int
-     */
-    public static int readInt(String prompt) {
-        while (true) {
-            try {
-                Scanner in = new Scanner(System.in);
-                System.out.println(prompt);
-                String numStr = in.next();
-                return Integer.parseInt(numStr);
-            } catch (NumberFormatException ex) {
-                System.out.println("Bad numeric string - Try again");
-            }
-        }
-    }
-    /*</listing>*/
+	/* <listing chapter="A" number="6"> */
+	/**
+	 * Method to return an integer data value.
+	 * 
+	 * @param prompt Message
+	 * @return The data value read as an int
+	 */
+	public static int readInt(String prompt) {
+		while (true) {
+			try {
+				Scanner in = new Scanner(System.in);
+				System.out.println(prompt);
+				String numStr = in.next();
+				return Integer.parseInt(numStr);
+			} catch (NumberFormatException ex) {
+				System.out.println("Bad numeric string - Try again");
+			}
+		}
+	}
+	/* </listing> */
 
-    /*<listing chapter="A" number="7">*/
-    /**
-     * Method to return an integer data value between two
-     * specified end points.
-     * @pre minN <= maxN.
-     * @param prompt Message
-     * @param minN Smallest value in range
-     * @param maxN Largest value in range
-     * @throws IllegalArgumentException
-     * @return The first data value that is in range
-     */
-    public static int readInt(String prompt, int minN, int maxN) {
-        if (minN > maxN) {
-            throw new IllegalArgumentException(
-                    "In readInt, minN " + minN
-                    + " not <= maxN " + maxN);
-        }
-        // Arguments are valid, read a number
-        boolean inRange = false; //Assume no valid number read
-        int n = 0;
-        Scanner in = new Scanner(System.in);
-        while (!inRange) {
-            try {
-                System.out.println(
-                        prompt + "\nEnter an integer between "
-                        + minN + " and " + maxN);
-                String line = in.nextLine();
-                n = Integer.parseInt(line);
-                inRange = (minN <= n && n <= maxN);
-            } catch (NumberFormatException ex) {
-                System.out.println("Bad numbric string - Try again");
-            }
-        }
-        return n;
-    }
-    /*</listing>*/
+	/* <listing chapter="A" number="7"> */
+	/**
+	 * Method to return an integer data value between two specified end points.
+	 * 
+	 * @pre minN <= maxN.
+	 * @param prompt Message
+	 * @param minN   Smallest value in range
+	 * @param maxN   Largest value in range
+	 * @throws IllegalArgumentException
+	 * @return The first data value that is in range
+	 */
+	public static int readInt(String prompt, int minN, int maxN) {
+		if (minN > maxN) {
+			throw new IllegalArgumentException("In readInt, minN " + minN + " not <= maxN " + maxN);
+		}
+		// Arguments are valid, read a number
+		boolean inRange = false; // Assume no valid number read
+		int n = 0;
+		Scanner in = new Scanner(System.in);
+		while (!inRange) {
+			try {
+				System.out.println(prompt + "\nEnter an integer between " + minN + " and " + maxN);
+				String line = in.nextLine();
+				n = Integer.parseInt(line);
+				inRange = (minN <= n && n <= maxN);
+			} catch (NumberFormatException ex) {
+				System.out.println("Bad numbric string - Try again");
+			}
+		}
+		return n;
+	}
+	/* </listing> */
 }
